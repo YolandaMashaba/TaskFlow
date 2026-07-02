a29cf64aeff2f2de5229d8ba4c4b158d4a6bf9ee
YolandaMashaba <nkokayolanda@gmail.com>
2026-07-02 14:08:57 +0200

    chore: update firebase env variables for CRA

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,12 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDd5Z9gA40SqDssiFCFAipcy-rsSrWuSQQ",
-  authDomain: "taskflow-e7a91.firebaseapp.com",
-  projectId: "taskflow-e7a91",
-  storageBucket: "taskflow-e7a91.firebasestorage.app",
-  messagingSenderId: "190080312133",
-  appId: "1:190080312133:web:bd79436827917b18c4d42c",
-  measurementId: "G-3H6V0QJS9W"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
